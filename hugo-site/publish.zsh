2ad3b7e313ce1024c46fd426d8e1174a5c0774cb
David Craddock <contact@davidcraddock.net>
2024-09-24 02:10:36 +0100

    Updated publish

--- hugo-site/publish.zsh
+++ hugo-site/publish.zsh
@@ -7,9 +7,11 @@ rm -rf public/*
 sudo chown -R david:users .
 git add -f .
 git commit -m "Latest hugo modification"
+snap run hugo server --disableFastRender
 snap run hugo
 git add -f .
 git commit -m "Latest hugo publish"
 git push origin main
-
+cd ~/.hqconfig/hugo-davidcraddocknet-blog
+./deploy-davidcraddocknet-hugo.sh
 
