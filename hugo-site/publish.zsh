f40c1de6ba7f3f6eed5ebec764db50c98b9949d6
David Craddock <contact@davidcraddock.net>
2025-03-03 17:48:34 +0000

    Tidy up

--- hugo-site/publish.zsh
+++ hugo-site/publish.zsh
@@ -11,6 +11,6 @@ hugo
 git add -f .
 git commit -m "Latest hugo publish" || echo ''
 git push origin main
-cd ~/.hqconfig/hugo-davidcraddocknet-blog
+cd ~/.hqconfig/swag-deploy/hugo-davidcraddocknet-blog
 ./deploy-davidcraddocknet-hugo.sh
 
