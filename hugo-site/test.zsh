68fc330094b3421b79b9a840050e18089ad5c19a
David Craddock <contact@davidcraddock.net>
2025-03-28 07:09:00 +0000

    Latest hugo modification

--- hugo-site/test.zsh
+++ hugo-site/test.zsh
@@ -3,7 +3,6 @@
 set -e
 set -x
 
-hugo server --buildDrafts
 hugo --gc --config ./hugo.yaml
-hugo server --config ./hugo.yaml --disableFastRender || echo ''
+hugo server --buildDrafts --config ./hugo.yaml --disableFastRender
 
